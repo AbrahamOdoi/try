e4a8bd5360998c13142255155ead4c26874cfe16
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-04 01:00:41 +0000

    ss
    Fd

---
+++ scripts/index.js
@@ -0,0 +1,70 @@
+﻿(function () {
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    var pictureSource;
+    var destinationType;
+
+    function onDeviceReady() {
+        pictureSource = navigator.camera.PictureSourceType;
+        destinationType = navigator.camera.DestinationType;
+
+//
+		$(function () {
+             "use strict";
+             $("#decode").click(function () {
+                 //alert("clicked");
+                 cordova.plugins.barcodeScanner.scan(
+                     function (result) {
+                         alert(result.text);
+                     },
+                     function (error) {
+                         alert(error);
+                     }
+                 );
+                 //end
+             });
+         });
+		//
+
+		//Geolocation start
+         var c = function (pos) {
+             var lat = pos.coords.latitude,
+                 long = pos.coords.longitude,
+                 coords = lat + ', ' + long;
+
+             document.getElementById('google-map').setAttribute('src', 'http://maps.google.co.uk?q=' + coords + '&z=60&output=embed');
+
+         }
+
+         //get location function
+         document.getElementById("getloc").onclick = function () {
+             navigator.geolocation.getCurrentPosition(c);
+             return false;
+         }
+
+
+        // document.getElementById("camera").onclick = function () {
+        //     navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        //         quality: 50,
+        //         destinationType: destinationType.DATA_URL,
+        //         saveToPhotoAlbum: true,
+        //         allowEdit: true
+        //     });
+        // }
+        //
+    };
+
+    function onPhotoDataSuccess(imageData) {
+
+    }
+
+    function onFail(message) {
+
+    }
+
+    function clearCache() {
+
+    }
+
+})();
+
